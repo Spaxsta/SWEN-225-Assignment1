c0993126df77ede43870b14395f7a0d15c9f2d78
Spaxsta <kenneddevo@myvuw.ac.nz>
2018-08-13 18:13:49 +1200

    Add files via upload

--- Cluedo.java
+++ Cluedo.java
@@ -1,51 +1,58 @@
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static java.util.Arrays.asList;  
+import static java.util.Arrays.asList;
+
+import java.awt.Point;  
 
 public class Cluedo {
 	public Cluedo() {
-		Square[][] board = new Square[24][25];
-		String[] output = {"#", "#", "#", "#", "#", "#", "#", "#", "#", "+", "#", "#", "#", "#", "S", "#", "#", "#", "#", "#", "#", "#", "#", "#", 
-				"R", "R", "R", "R", "R", "S", "+", "+", "+", "+", "R", "R", "R", "R", "+", "+", "+", "+", "R", "R", "R", "R", "R", "R", 
-				"R", "R", "R", "R", "R", "R", "+", "+", "R", "R", "R", "R", "R", "R", "R", "R", "+", "+", "R", "R", "R", "R", "R", "R",
-				"R", "R", "R", "R", "R", "R", "+", "+", "R", "R", "R", "R", "R", "R", "R", "R", "+", "+", "R", "R", "R", "R", "R", "R",
-				"R", "R", "R", "R", "R", "R", "+", "+", "R", "R", "R", "R", "R", "R", "R", "R", "+", "+", "E", "R", "R", "R", "R", "R",
-				"R", "R", "R", "R", "R", "R", "+", "+", "E", "R", "R", "R", "R", "R", "R", "E", "+", "+", "+", "R", "R", "R", "S", "#",
-				"R", "R", "R", "R", "E", "R", "+", "+", "R", "R", "R", "R", "R", "R", "R", "R", "+", "+", "+", "+", "+", "+", "+", "+",
-				"+", "+", "+", "+", "+", "+", "+", "+", "R", "E", "R", "R", "R", "R", "E", "R", "+", "+", "+", "+", "+", "+", "+", "#",
-				"#", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "R", "R", "R", "R", "R", "R",
-				"R", "R", "R", "R", "R", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "E", "R", "R", "R", "R", "R",
-				"R", "R", "R", "R", "R", "R", "R", "R", "+", "+", "#", "#", "#", "#", "#", "+", "+", "+", "R", "R", "R", "R", "R", "R",
-				"R", "R", "R", "R", "R", "R", "R", "R", "+", "+", "#", "#", "#", "#", "#", "+", "+", "+", "R", "R", "R", "R", "R", "R",
-				"R", "R", "R", "R", "R", "R", "R", "E", "+", "+", "#", "#", "#", "#", "#", "+", "+", "+", "R", "R", "R", "R", "E", "R",
-				"R", "R", "R", "R", "R", "R", "R", "R", "+", "+", "#", "#", "#", "#", "#", "+", "+", "+", "+", "+", "+", "+", "+", "#",
-				"R", "R", "R", "R", "R", "R", "R", "R", "+", "+", "#", "#", "#", "#", "#", "+", "+", "+", "R", "R", "E", "R", "R", "#",
-				"R", "R", "R", "R", "R", "R", "E", "R", "+", "+", "#", "#", "#", "#", "#", "+", "+", "R", "R", "R", "R", "R", "R", "R",
-				"#", "+", "+", "+", "+", "+", "+", "+", "+", "+", "#", "#", "#", "#", "#", "+", "+", "E", "R", "R", "R", "R", "R", "R",
-				"+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "+", "R", "R", "R", "R", "R", "R", "R",
-				"#", "+", "+", "+", "+", "+", "+", "+", "+", "R", "R", "E", "E", "R", "R", "+", "+", "+", "R", "R", "R", "R", "R", "#",
-				"S", "R", "R", "R", "R", "R", "E", "+", "+", "R", "R", "R", "R", "R", "R", "+", "+", "+", "+", "+", "+", "+", "+", "+",
-				"R", "R", "R", "R", "R", "R", "R", "+", "+", "R", "R", "R", "R", "R", "E", "+", "+", "+", "+", "+", "+", "+", "+", "#",
-				"R", "R", "R", "R", "R", "R", "R", "+", "+", "R", "R", "R", "R", "R", "R", "+", "+", "E", "R", "R", "R", "R", "R", "S",
-				"R", "R", "R", "R", "R", "R", "R", "+", "+", "R", "R", "R", "R", "R", "R", "+", "+", "R", "R", "R", "R", "R", "R", "R",
-				"R", "R", "R", "R", "R", "R", "R", "+", "+", "R", "R", "R", "R", "R", "R", "+", "+", "R", "R", "R", "R", "R", "R", "R",          
-				"R", "R", "R", "R", "R", "R", "#", "+", "#", "R", "R", "R", "R", "R", "R", "+", "+", "R", "R", "R", "R", "R", "R", "R"};
-		int pos = 0;
-		for(int i = 0; i < 23;i++) {
-			for(int j = 0; j < 24;j++) {
-				pos = j+(i*24);
-				if(output[pos].equals("R")) {
-					board.add(new Room(false));
-				} else if(output[pos].equals("+")) {
-					board.add(new FloorTile());
-				} else if(output[pos].equals("+")) {
-					board.add(new Wall());
-				}
-				System.out.print(output[pos]);
+		Map<Point, String> map = new HashMap<Point, String>();
+		map.put(new Point(4,6), "Kitchen");
+		
+		map.put(new Point(8,5), "Ball Room");
+		map.put(new Point(9,7), "Ball Room");
+		map.put(new Point(14,7), "Ball Room");
+		map.put(new Point(15,5), "Ball Room");
+		
+		map.put(new Point(18,4), "Conservatory");
+		
+		map.put(new Point(18,9), "Billard Room");
+		map.put(new Point(22,12), "Billard Room");
+		
+		map.put(new Point(20,14), "Library");
+		map.put(new Point(17,16), "Library");
+		
+		map.put(new Point(17,21), "Study");
+		
+		map.put(new Point(12,18), "Hall");
+		map.put(new Point(11,18), "Hall");
+		map.put(new Point(14,20), "Hall");
+		
+		map.put(new Point(6,19), "Lounge");
+		
+		map.put(new Point(6,15), "Dining Room");
+		map.put(new Point(7,12), "Dining Room");
+		
+		public void makeAccusation() {
+			System.out.println("Accuse a character");
+			String characterGuess = sc.next();
+			System.out.println("Accuse a weapon");
+			String weaponGuess = sc.next();
+			System.out.println("Accuse a room");
+			String roomGuess = sc.next();
+			gameWon(characterGuess, weaponGuess, roomGuess);
+			
+		}
+		
+		public void gameWon(String characterGuess, String weaponGuess, String roomGuess) {
+			if(solution.get(0).equals(characterGuess) && solution.get(1).equals(weaponGuess) && solution.get(2).equals(roomGuess)) {
+				won = true;
 			}
-			System.out.println("");
+			
 		}
 	}
 
